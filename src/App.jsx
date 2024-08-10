a239a5c15a16662cddc2f9a74f11dc83540e7b6b
rupesh <rpiwal02@gmail.com>
2024-08-10 14:13:26 +0530

    upload comp UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Upload from "./components/Upload";
 
 const App = () => {
-  return <div>Upload</div>;
+  return (
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center mx-auto">
+      <Upload />
+    </div>
+  );
 };
 
 export default App;
